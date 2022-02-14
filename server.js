48b02d30cb2c58b33614bc8db4b3d3fd53394e16
akpathak1007 <anujkumarpathak1007@gmail.com>
2022-02-14 13:55:11 +0530

    stream app api server

---
+++ server.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5,];
+let otherarr = [];
+let obj = {
+  first: 'midnal',
+  last: 'sharma',
+  age: 22
+}
+
+for (let i in arr) {
+  console.log('hello');
+  console.log(i);
+}
